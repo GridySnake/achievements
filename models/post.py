cc60f46b26e29e3e48721daa92537b13647a08fc
Mamedov-Zi <mamedovziyar@gmail.com>
2021-10-05 23:25:07 +0300

    Changes for new db

--- models/post.py
+++ models/post.py
@@ -2,7 +2,6 @@ import asyncpg
 import datetime
 from config.common import BaseConfig
 connection_url = BaseConfig.database_url
-# TODO: change for new db
 
 class Post:
     @staticmethod
@@ -22,7 +21,8 @@ class Post:
         }
         await conn.execute(f"""
                         insert INTO posts (post_id, user_id, community_id, course_id, message, date_created) values(
-                        {data['post_id']}, {data['user_id']}, {data['community_id']}, {data['course_id']}, '{data['message']}', '{data['date_created']}')
+                        {data['post_id']}, {data['user_id']}, {data['community_id']}, {data['course_id']}, 
+                        '{data['message']}', '{data['date_created']}')
                         """)
 
     @staticmethod
@@ -33,12 +33,39 @@ class Post:
         conn = await asyncpg.connect(connection_url)
         if user_id:
             posts = await conn.fetch(f"""
-                SELECT p.user_id, p.message, , u.last_name, u.first_name
+                SELECT p.user_id, p.message, 
+                        u.name, u.surname, im.href
                 FROM posts as p
-                INNER JOIN users as u ON u.id = p.user_id
+                INNER JOIN users_information as u 
+                    ON u.user_id = p.user_id
+                INNER JOIN images as im 
+                    on im.image_id = p.image_id
                 WHERE p.user_id = {user_id}
                 LIMIT {limit}
                 """)
             return posts
-        # if community_id:
-
+        if community_id:
+            posts = await conn.fetch(f"""
+                select p.community_id, p.message,
+                    com.community_name, im.href
+                from posts as p
+                inner join communities as com
+                    on com.community_id = p.community_id
+                inner join images as im 
+                    on im.image_id = p.image_id
+                WHERE p.user_id = {community_id}
+                LIMIT {limit}
+            """)
+            return posts
+        if course_id:
+            posts = await conn.fetch(f"""
+                select p.course_id, p.message,
+                    cour.course_name, im.href
+                from posts as p
+                inner join courses as cour
+                    on cour.course_id = p.course_id
+                inner join images as im 
+                    on im.image_id = p.image_id
+                WHERE p.user_id = {course_id}
+            """)
+            return posts
