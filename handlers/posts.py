f8e2b6e28a61c937965ad2857a751d584a8d1664
GridySnake <kunilov-aleksandr2012@mail.ru>
2021-10-26 00:35:19 +0300

    commit_27

--- handlers/posts.py
+++ handlers/posts.py
@@ -10,6 +10,8 @@ class PostView(web.View):
     async def post(self):
         data = await self.post()
         session = await get_session(self)
+        if 'user' not in self.session:
+            return web.HTTPForbidden()
         if 'user' in session and data['message'] and not data['file']:
             await Post.create_post(user_id=session['user']['id'], message=data['message'])
         if 'user' in session and data['message'] and data['file']:
@@ -20,4 +22,4 @@ class PostView(web.View):
             await Post.create_post(user_id=session['user']['id'], message=data['message'], image_href=data_file.filename)
             location = str(f"/{session['user']['id']}")
             return web.HTTPFound(location=location)
-        return web.HTTPForbidden()
+
