b1248895f0ed4eb635e791d5ca88344f1518e4bc
GridySnake <kunilov-aleksandr2012@mail.ru>
2021-10-30 00:24:47 +0300

    commit_31

---
+++ shpi.py
@@ -0,0 +1,25 @@
+import chessdotcom
+
+user = 'crazyniga1917'
+
+profile = chessdotcom.get_player_profile(user).json['player']
+profile_current_games = chessdotcom.get_player_current_games(user)
+profile_clubs = chessdotcom.get_player_clubs(user).json
+profile_tournaments = chessdotcom.get_player_tournaments(user)
+profile_stats_modes = chessdotcom.get_player_stats(user).json['stats']
+profile_chess_rapid_last = profile_stats_modes['chess_rapid']['last']
+profile_chess_rapid_best = profile_stats_modes['chess_rapid']['best']
+profile_chess_rapid_stats = profile_stats_modes['chess_rapid']['record']
+profile_chess_blitz_last = profile_stats_modes['chess_blitz']['last']
+profile_chess_blitz_stats = profile_stats_modes['chess_blitz']['record']
+profile_fide = profile_stats_modes['fide']
+profile_tactics_highest = profile_stats_modes['tactics']['highest']
+profile_tactics_lowest = profile_stats_modes['tactics']['lowest']
+profile_lessons = profile_stats_modes['lessons']  # todo: add parameters
+puzzle_rush = chessdotcom.get_player_stats(user).json['stats']['puzzle_rush']['best']
+grand_masters = chessdotcom.get_titled_players('GM').json['players']  # todo: others titles
+leaderboards = chessdotcom.get_leaderboards().json['leaderboards']['daily']  # todo: others features
+
+print(profile_tactics_lowest)
+
+#  todo: logic = verify button
