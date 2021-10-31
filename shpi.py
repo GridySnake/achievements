9034ff3aec59e6206a236da44620c7c69eda2f68
GridySnake <kunilov-aleksandr2012@mail.ru>
2021-10-31 23:47:07 +0300

    commit_35

--- shpi.py
+++ shpi.py
@@ -1,54 +1,11 @@
 import twitch
 from config.common import BaseConfig
+helix = twitch.Helix(BaseConfig.twitch_client_id, BaseConfig.twitch_client_secret)
 
-
-class Twitch:
-    helix = twitch.Helix(BaseConfig.twitch_client_id, BaseConfig.twitch_client_secret)
-
-    @staticmethod
-    def get_user_info(username):
-        return helix.user(username).data
-
-    @staticmethod
-    def get_user_video_info(username):
-        return [video.data for video in helix.user(username).videos()]
-
-    @staticmethod
-    def get_user_followers_names(username):
-        return [follower.from_name for follower in helix.user(username).following()]
-
-    @staticmethod
-    def get_user_followers_data(username):
-        return [follower.data for follower in helix.user(username).following()]
-
-    @staticmethod
-    def get_user_following_names(username):
-        return [following.from_name for following in helix.user(username).following()]
-
-    @staticmethod
-    def get_user_following_data(username):
-        return [following.data for following in helix.user(username).following()]
-
-    @staticmethod
-    def is_online(username):
-        return helix.user(username).is_live
-
-    @staticmethod
-    def get_top_games():
-        return [i.data for i in helix.top_games()]
-
-    @staticmethod
-    def get_top_games_names():
-        return [i.name for i in helix.top_games()]
-
-    @staticmethod
-    def get_streams():
-        return [i.data for i in helix.streams()]
-
-    @staticmethod
-    def get_streams_names():
-        return [i.data for i in helix.streams()]
-
+from TikTokAPI import TikTokAPI
+api = TikTokAPI()
+user_obj = api.getUserByName("fcbarcelona")
+print(user_obj)
 
 client_id = 'blif82wvvraojfm84xp82fam1ljnii'
 client_secret = 'abqm6gspq4lhaddede83szum1u0v2b'
